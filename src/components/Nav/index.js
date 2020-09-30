968c5d0aa6923a9855e9310f9de5a9f8e6cc9f63
Sean Cone <seandoesitbetter@gmail.com>
2020-09-30 11:55:07 -0700

    20.3.5

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import React, { useEffect } from 'react';
 
 function Nav(props) {
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+      }, [currentCategory]);
   const {
     categories = [],
     setCurrentCategory,
